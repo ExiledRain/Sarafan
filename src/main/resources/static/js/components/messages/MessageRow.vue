a78526d797d41fac0b17ebe7e57f84545b4ef637
ExiledRain <gett_lostt@bk.ru>
2021-01-07 20:43:30 +0200

    Added Vuetify base design + icons

--- src/main/resources/static/js/components/messages/MessageRow.vue
+++ src/main/resources/static/js/components/messages/MessageRow.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    <i>({{ message.id }})</i> {{ message.text }}
-    <span style="position: absolute; right: 0">
-            <input type="button" value="Edit" @click="edit" />
-            <input type="button" value="X" @click="del" />
-        </span>
-  </div>
+  <v-card class="my-2">
+    <v-card-text primary-title>
+      <i>({{ message.id }})</i>
+      {{ message.text }}
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="edit" small flat round>
+        <v-icon>text_rotation_none</v-icon>
+      </v-btn>
+      <v-btn @click="del">
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
